0cea77b26a438c6b23678cc2d88ed7484834be3c
Karan Nedungadi <karannedungadi@my.capilanou.ca>
2018-04-06 21:42:57 +0000

    have implemented libraries and user input

---
+++ wordcloud.c
@@ -0,0 +1,68 @@
+/*
+ *  Comp220 - wordcloud.c
+ *    
+ *
+ *  Author: Tim Wriglesworth, Karan Nedungadi
+ *  Date: Mar. 2017 // edited in April
+ */
+ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+
+//function prototypes
+FILE* open (char*);
+
+
+
+
+
+int main (int argc, char* argv[])  
+{
+	
+char* filename;          //get user input
+filename = argv[1];
+	
+	if (argc <2)
+	{
+		printf("usage error: filename.txt");   // print some usage handling
+		exit(-1);
+	}
+   FILE* f = open(filename);     //call my function
+   
+   
+   
+   
+   
+   
+   
+   
+   fclose(f);
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+  
+  
+  
+   FILE* open(char* filename)     //open the file and check existence
+	{
+	   FILE* file = fopen(filename, "r");
+	   if (file == NULL) {
+		  printf("Error: file does not exist: %s \n", filename);
+		  exit(-1);
+	   }
+	   return file;
