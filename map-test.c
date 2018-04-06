1f886c2cf73ac752b529596efefeed3d216bfb27
Keaton <keatondirect@icloud.com>
2018-04-06 17:57:32 +0000

    Added some basic tests, the naming conventiosn seem to be weird across all files, try make all and see.

--- map-test.c
+++ map-test.c
@@ -11,7 +11,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
-#include <map.h>
+#include "map.h"
+
 
 int main ()
 {
@@ -19,8 +20,32 @@ int main ()
     map_t map = mapCreate();
     assert(mapIsEmpty(map));
     
+    entry_t entryA = {"a",1};
+    entry_t entryB = {"b",2};
+    entry_t entryC = {"c",3};
+    entry_t entryD = {"d",4};
+
     //INSERT
+    mapInsert(map,entryA);
+    mapInsert(map,entryB);
+    mapInsert(map,entryC);
+    mapInsert(map,entryD);
     
+    assert(mapSize(map) == 4);
+    
+    assert(mapGet(map,"a") == 1);
+    assert(mapGet(map,"b") == 2);
+    assert(mapGet(map,"c") == 3);
+    assert(mapGet(map,"d") == 4);
+
+    assert(mapHasKey(map,"a"));
+    assert(mapHasKey(map,"b"));
+    assert(mapHasKey(map,"c"));
+    assert(mapHasKey(map,"d"));
+
+
+
+
  
     //DELETE MAP
     mapClear(*map);
