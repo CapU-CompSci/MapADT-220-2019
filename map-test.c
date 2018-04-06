6730a54cac3009772452f8db959895eb46c32dba
Robert A Bell <robertbell4@my.capilanou.ca>
2018-04-06 00:45:55 +0000

    fixed the type of key

---
+++ map-test.c
@@ -0,0 +1,18 @@
+/*
+ *  Comp220 - Test Driver which will work for EITHER treemap and hashmap
+ *  
+ *
+ *  Author: Robert Bell
+ *  Date:  April
+ */
+ 
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+int main ()
+{
+    return 0;
+}
