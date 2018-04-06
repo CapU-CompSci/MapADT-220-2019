59a5c0b49417a48d517a0773957561105f64a785
Robert A Bell <robertbell4@my.capilanou.ca>
2018-04-06 00:49:31 +0000

    fixed the type of key

--- map.h
+++ map.h
@@ -36,7 +36,7 @@ void mapInsert( map_t* map, entry_t map);
 * POST: HasKey(key) == false
 */
 
-void mapDelete(bstNode_t* map, keytype key); //differnt type names between keytype and char* key....
+void mapDelete(map_t map, keytype key); //differnt type names between keytype and char* key....
 
 
 /*
