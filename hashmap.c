80ea8c4d8f4b00830f4d0ae9230a98344c4125c3
Joseph Fall <jfall@capilanou.ca>
2018-04-08 06:50:36 +0000

    Fix for #69 - compilation errors

--- hashmap.c
+++ hashmap.c
@@ -53,7 +53,7 @@ int mapindex(keytype Key)
  * Author: Greagorey Markerian
  * Needs to be reviewed and tested
  */
-void mapInsert(map_t* map, keytype key, valuetype value){
+void mapInsert(map_t* mapref, keytype key, valuetype value)
 {
     map_t map = *mapref;
     int index = mapindex(key);
@@ -63,8 +63,8 @@ void mapInsert(map_t* map, keytype key, valuetype value){
         counter++;
         assert(counter < COL_SIZE); // TODO: better error handling required for column overflow.
     }
-    entry_t entry = {key, value}; // TODO: an Entry constructor would help here.
-    map->hashtable[index][counter]=entry;
+    // TODO: CRITICAL - an Entry constructor to allocate dynamic memory for deep-copy of key!
+    map->hashtable[index][counter] = {key, value};
 }
 
 /*
@@ -75,7 +75,7 @@ void mapInsert(map_t* map, keytype key, valuetype value){
 map_t mapCreate()
 {
     map_t map;
-    map = calloc(1,sizeof(Hmap)); //im assuming this sets all values to NULL
+    map = calloc(1,sizeof(Hmap)); // assumes all hashtable values set to NULL
     return map;
 }
     
@@ -86,7 +86,7 @@ map_t mapCreate()
  */
 int mapGet(map_t map, keytype key)
 {
-    if(mapHasKey(map, key)) {
+    if(mapHasKey(map, key)) {  // PRE-condition -- just assert it.
         int index = mapindex(key);
         int counter = 0;
 
@@ -96,6 +96,7 @@ int mapGet(map_t map, keytype key)
         return map->hashtable[index][counter].value;
     }
 
+    // TODO: simply assert PRE-condition at top of function.
     printf("ERROR: key does not exist within map");
     return 0;
 }
@@ -106,8 +107,9 @@ int mapGet(map_t map, keytype key)
  * Needs to be reviewed and tested
  */
  
-void mapRemove(map_t* map, keytype key)
+void mapRemove(map_t* mapref, keytype key)
 {
+    map_t map = *mapref;
     if (!mapHasKey(map, key)) {
         return;
     }
@@ -116,10 +118,12 @@ void mapRemove(map_t* map, keytype key)
         int i = mapindex(key);
    
         while(map->hashtable[i][c].key!=NULL) {
-            if(map->hashtable[index][counter].key == key) {
+            if(map->hashtable[i][c].key == key) {
+                ; /*  This code is just plain wrong! The map struct has one member: a hashtable.
                 free(map->key);
                 free(map->value);
                 map_t = {NULL, NULL}
+                */
             }
             c++;
         }
@@ -151,14 +155,17 @@ bool mapHasKey(map_t map, keytype key)
  * Last Changed: 2018/04/05
  * Needs to be reviewed and tested
  */
-void mapClear(map_t * map)
+void mapClear(map_t* mapref)
 {
+    map_t map = *mapref;
     int i, c;
     for(i = 0; i < ARRAY_SIZE; i++) {
         for(c = 0; c < COL_SIZE; c++) {
+            ; /*  This code is just plain wrong! The map struct has one member: a hashtable.
             free(map->key);
             free(map->value);
             map_t = {NULL, NULL}
+            */
         }
     }
 }
@@ -173,7 +180,7 @@ int mapSize(map_t map)
 {
     int i, c, size = 0;
     for(i=0; i<ARRAY_SIZE; i++) {
-        for(c=0;map[i][c]!=NULL;c++) {
+        for(c=0; map->hashtable[i][c].key!=NULL; c++) {
             size++;
         }
     }
