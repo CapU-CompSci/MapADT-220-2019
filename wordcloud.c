12e2995758cb0d77df8c93e8834da8cd9c7e039a
Joseph Fall <jfall@capilanou.ca>
2018-04-08 06:00:58 +0000

    Fix for #71 - compilation errors

--- wordcloud.c
+++ wordcloud.c
@@ -2,13 +2,14 @@
  *  Comp220 - wordcloud.c
  *    
  *
- *  Author: Tim Wriglesworth, Karan Nedungadi
- *  Date: Mar. 2017 // edited in April 06
+ *  Author: Tim Wriglesworth, Karan Nedungadi, Joseph Fall
+ *  Date: Apr. 2017
  */
  
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <assert.h>
 
 #ifdef HASHMAP
@@ -28,49 +29,50 @@
 
 //function prototypes
 FILE* open (char*);
-void nodePrint (bstNode_t Node);
+// void nodePrint (bstNode_t Node);
 void inorderPrint(map_t treemap);
 void removePunc(char* word);
 void shiftLeft(char* word, int index);
 
+#define MAX_WORD_LENGTH 127
+#define WORD_FORMAT "%127s"  // must match MAX_WORD_LENGTH
 
 int main (int argc, char* argv[])  
 {
-	
-char* filename; //get user input
-filename = argv[1];
-	
 	if (argc <2){
 		printf("usage error: filename.txt");   // print some usage handling
 		exit(-1);
 	}
+	char* filename; //get user input
+	filename = argv[1];
 	FILE* f = open(filename);     //call my function
 	
-	wordtree = mapCreate();
-	char* word;
-	int count = 1;
+	map_t wordmap = mapCreate();
+	char word[MAX_WORD_LENGTH + 1];
 	
-	while(fscanf(f, " %127s", &word) == 1){
-		removePunc(word);
-		mapInsert(wordtree, word, count);  //still needs work
-		}
+	while(fscanf(f, WORD_FORMAT, word) == 1) {
+		int count = 1;
+		// TODO: if word already in map, get existing count, increment, then insert
+		mapInsert(&wordmap, word, count);  
+	}
 		
-		printf("WordCloud Frequency \n");
-		//print tabular form needs formatting..
-		inorderPrint(wordtree);
-		
-		fclose(f);
-		printf("\nIn order Finished...")
+	printf("WordCloud Frequency \n");
+	// TODO: print tabular form needs formatting...
+	
+	// This idea won't work:  "InOrder" is a tree concept - Maps are like Sets, they don't have an order!
+	// Besides, even in the treemap implementation, it is sorted in alphabetic order, not frequency order!
+	inorderPrint(wordmap);
+	
+	fclose(f);
+	printf("\nIn order Finished...");
 }
 
 //removes all punction from input string
 void removePunc(char* word){
  	int i;
- 	char c = word[0]; //first character
- 	
  	for(i=0; i<strlen(word); i++){
- 		if (c == ispunc(word[i]))
- 		shiftLeft(word, i);
+ 		if ( ispunc(word[i])) 
+ 			shiftLeft(word, i);
  	}
 }
 
@@ -80,27 +82,29 @@ void removePunc(char* word){
 void shiftLeft(char* word, int index){
 	int i;
 	for (i=index; i<strlen(word); i++){
-		word[i]= word[i+1]
+		word[i]= word[i+1];
 	}
 }
 
 
 //prints the binary search tree from lowest to largest key value
 void inorderPrint(map_t treemap){
-   	
+	printf ("Not implemented yet!\n");
+   	/*  This uses non-public members of tree implementation -- not part of Map API!
    	if(!mapIsEmpty(treemap)){
    		inorderPrint(tree->left)
    		nodePrint(*treemap);
    		inorderPrint(tree->right);
    	}
+   	*/
 }
-
+/*  Implementation details of treemap do not belong here!
 // prints the key-value pair for the respective node in the tree
 void nodePrint (bstNode_t Node){
 	printf("---%s" node.entry.key);
 	printf("---%d",node.entry.value);
 }
-
+*/
 
 //open the file and check existence
 FILE* open(char* filename){
