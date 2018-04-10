d3feca8370a948ab4f4e575a928eb144a22e8d8a
Keaton <keatondirect@icloud.com>
2018-04-10 16:29:24 +0000

    Uncommented test driver, #90 commented out

--- map-test.c
+++ map-test.c
@@ -24,40 +24,40 @@ int main ()
     //MAP CREATION
     map_t map = mapCreate();
     assert(mapSize(map)==0);
-    
-    printf("test");
-
-    //FIND INSERTION POINT
-    
-    
+    printf("Testing...\n");
 
     //INSERT
-    mapInsert(&map,"a", 1);
+    mapInsert(&map,"a",1);
     mapInsert(&map,"b",2);
     mapInsert(&map,"c",3);
     mapInsert(&map,"d",4);
-    
+    printf("mapInsert works\n");
+
     //SIZE
-    printf("The map contains %d keypairs", mapSize(map));
+    printf("The map contains %d keypairs\n", mapSize(map));
     assert(mapSize(map) == 4);
-/*    
+    
     //GET
     assert(mapGet(map,"a") == 1);
     assert(mapGet(map,"b") == 2);
     assert(mapGet(map,"c") == 3);
     assert(mapGet(map,"d") == 4);
-
+    printf("mapGet works\n");
+    
     //HASKEY?
     assert(mapHasKey(map,"a"));
     assert(mapHasKey(map,"b"));
     assert(mapHasKey(map,"c"));
     assert(mapHasKey(map,"d"));
+    printf("mapHasKey works\n");
+
+    //REMOVE
+    //mapRemove(&map,"b");
+    //assert(!mapHasKey(map,"a"));
+    //assert(mapSize(map) == 3);
     
-    mapRemove(&map,"a");
-    assert(!mapHasKey(map,"a"));
-    assert(mapSize(map) == 3);
     //DELETE MAP
     mapClear(&map);
     assert(mapSize(map)==0);
-    */
+    
 } 
