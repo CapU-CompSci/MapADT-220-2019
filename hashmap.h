0ec799be3e8b4d15d5b6979375bb36642e421ad1
Grace Humer <gracehumer@my.capilanou.ca>
2019-04-02 00:29:46 +0000

    #135 added remove function to hashmap.h

--- hashmap.h
+++ hashmap.h
@@ -10,3 +10,9 @@
  * sets the value for key if HasKey(key), otherwise inserts a new value in Map
  */
  void Insert(char* key, int value);
+ 
+/*
+ * POST: HasKey(key) == false
+ * removes the (key, value) pair from the Map, no effect if !HasKey(key)
+ */
+void Remove(char* key);
