d242dd6c18be4a1b34abdba8d9f370c97444a635
Gus <gustavoschiele@my.capilanou.ca>
2019-04-02 00:29:56 +0000

    #132 added Remove

--- treemap.h
+++ treemap.h
@@ -14,3 +14,10 @@
  *Sets the value for key if HasKey(key), otherwise inserts a new value in Map
  */
 void Insert(char* key,int value);
+
+
+/*
+ *POST: HasKey(key) == false
+ *removes the (key, value) pair from the Map, no effect if !HasKey(key)
+ */
+void Remove(char* key);
