46e03b310a7c1c2f2dede18565de970976f03054
Alex Verga <alessandroverga@my.capilanou.ca>
2019-04-02 00:01:09 +0000

    #132 added prototpe or insert function

---
+++ treemap.h
@@ -0,0 +1,16 @@
+/*
+ *
+ * COMP 220 Project 4: Public API for TreeMap ADT
+ * Author: Cap Comp220 Spring 2019
+ * Date: April 1, 2019
+ *
+ */
+
+
+
+
+/*
+ *POST: Get(key) == value
+ *Sets the value for key if HasKey(key), otherwise inserts a new value in Map
+ */
+void Insert(char* key,int value);
