a3a1f3ef5ffd263a441fe77f323d4d61032ea324
Grace Humer <gracehumer@my.capilanou.ca>
2019-04-08 23:37:16 +0000

    #141 added prototypes to treemap.h from hashmap.h

--- treemap.h
+++ treemap.h
@@ -35,3 +35,43 @@ void Remove(char* key);
  *Returns the value associated with the given key
  */
 int Get(char* key);
+
+/*
+ * POST: Get(key) == value()
+ * Sets the value for key if HasKey(key), otherwise inserts a new value in Map
+ */
+ void Insert(char* key, int value);
+ 
+/*
+ * POST: HasKey(key) == false
+ * Removes the (key, value) pair from the Map, no effect if !HasKey(key)
+ */
+void Remove(char* key);
+
+/* 
+ * PRE: HasKey(key)
+ * Returns the value associated with the given key
+ */
+int Get(char* key);
+
+/* 
+ * Returns true if the Map contains the given key
+ */
+bool HasKey(char* key);
+
+/*
+ * Returns the number of (key, value) pairs in the Map
+ */
+int Size(void);
+
+/*
+ * POST: Size() == 0
+ * Removes all items from the Map
+ */
+void Clear(void);
+
+/*
+ * Returns a dynamic array containing all the Map Keys (in any sequence)
+ * Returned array must be freed by client
+ */
+char** KeySet(void);
