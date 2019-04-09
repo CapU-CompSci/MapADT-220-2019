07c70e9f8f23e15f6998e74abad541aeaa81bbd0
Nima <nimakermanian@my.capilanou.ca>
2019-04-09 22:58:45 +0000

    #172 coded findKey

---
+++ TreeMap/bsTree.c
@@ -0,0 +1,71 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+btNode* bstFindkey(btNode root, char* key) {
+    /*
+    *   If strcmp returns a negative number, it means the 
+    *   first string is bigger, ex: first string is aaa
+    *   and the second value is zzz
+    */
+    
+    if ( strcmp(root.key, key) == 0 ){
+        return root;
+    }
+    
+    if (strcmp(root.key, key) < 0) {
+        return bstFindkey(root -> left, key);
+    }
+    
+    if (strcmp(root.key, key) > 0) {
+        return bstFindkey(root -> right, key);
+    }
+    
+    else //Key does not exist
+        return NULL;
+}
