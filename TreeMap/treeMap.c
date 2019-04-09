c483fdfe3957d93d33dc5fb4c368e7899624cae6
Grace Humer <gracehumer@my.capilanou.ca>
2019-04-09 21:18:08 +0000

    #160 copied treemap.h prototypes into treemap.c

--- TreeMap/treeMap.c
+++ TreeMap/treeMap.c
@@ -11,7 +11,73 @@
  #include <string.h>
  #include "treemap.h"
  
+ /*
+ * POST: Get(key) == value()
+ * Sets the value for key if HasKey(key), otherwise inserts a new value in Map
+ */
+ void Insert(char* key, int value){
+  
+ }
  
+/*
+ * POST: HasKey(key) == false
+ * Removes the (key, value) pair from the Map, no effect if !HasKey(key)
+ */
+void Remove(char* key){
+ 
+}
+
+/* 
+ * PRE: HasKey(key)
+ * Returns the value associated with the given key
+ */
+int Get(char* key){
+ 
+}
+
+/* 
+ * Returns true if the Map contains the given key
+ */
+bool HasKey(char* key){
+ 
+}
+
+/*
+ * Returns the number of (key, value) pairs in the Map
+ */
+int Size(bsTree* root){
+ 
+}
+
+/*
+ * POST: Size() == 0
+ * Removes all items from the Map
+ */
+void Clear(bsTree*){
+ 
+}
+
+/*
+ * Returns a dynamic array containing all the Map Keys (in any sequence)
+ * Returned array must be freed by client
+ */
+char** KeySet(bsTree* root){
+ 
+}
+
+/* 
+ * Return pointer to a new root node.
+ */
+bsTree Constructor(void){
+ 
+}
+
+/*
+ * No Return, destroys map.
+ */
+void Destructor(bsTree* root){
+ 
+}
  
  
  
