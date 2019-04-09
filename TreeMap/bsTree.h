a6da038d258c2166b4d9f5e9c1eb112f75293418
Gus <gustavoschiele@my.capilanou.ca>
2019-04-09 21:17:49 +0000

    removed junk

--- TreeMap/bsTree.h
+++ TreeMap/bsTree.h
@@ -1,14 +1,9 @@
 /*
-<<<<<<< HEAD
  *
  * COMP 220 Project 4: Private API for bsTree ADT
  * Author: Cap Comp220 Spring 2019
  * Date: April 1, 2019
  *
- *
-=======
- * Binary Search Tree Interface
- * Comp220 project 4
  */
 struct btNode_s {
 
@@ -23,4 +18,4 @@ struct bsTree_s {
     btNode* root;
 };
 typedef struct bsTree_s bsTree;
->>>>>>> c401058513038f5d9630aa1e90997b69288d164e
+
