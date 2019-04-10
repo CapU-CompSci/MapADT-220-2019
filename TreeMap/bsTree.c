513ba45f625a900b6d232cb970ee062b7c0effbd
Nima <nimakermanian@my.capilanou.ca>
2019-04-10 07:02:05 +0000

    #174 changed some of findkey in bsTree.c

--- TreeMap/bsTree.c
+++ TreeMap/bsTree.c
@@ -6,6 +6,12 @@
 
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+#include "bsTree.h"
 
 
 
@@ -45,24 +51,22 @@
 
 
 
-
-
-btNode* bstFindkey(btNode root, char* key) {
+btNode* bstFindkey(btNode* root, char* key) {
     /*
     *   If strcmp returns a negative number, it means the 
     *   first string is bigger, ex: first string is aaa
     *   and the second value is zzz
     */
     
-    if ( strcmp(root.key, key) == 0 ){
+    if ( strcmp(root -> key, key) == 0 ){
         return root;
     }
     
-    if (strcmp(root.key, key) < 0) {
+    if (strcmp(root -> key, key) < 0) {
         return bstFindkey(root -> left, key);
     }
     
-    if (strcmp(root.key, key) > 0) {
+    if (strcmp(root -> key, key) > 0) {
         return bstFindkey(root -> right, key);
     }
     
